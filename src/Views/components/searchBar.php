4ea3e4c289e843cbb1738db246a6c0ee8a54daab
felipe <felipe>
2025-03-13 19:44:49 -0300

    feat: Adiciona compomente `SearchBar`.

---
+++ src/Views/components/searchBar.php
@@ -0,0 +1,9 @@
+<div class="search-box">
+    <form method="GET" action="">
+        <label for="ipt-search">&#x1F50E;</label>
+        <input type="search" name="q" id="ipt-search" value="<?= $search ?>" placeholder="<?= $placeholder ?>" />
+        <div class="button-wrapper">
+            <button type="submit">Pesquisar</button>
+        </div>
+    </form>
+</div>
