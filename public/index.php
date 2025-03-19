567c324a7c278728dcc04aa3de7752d6f4e48549
felipe <felipe>
2025-03-19 20:02:14 -0300

    feat: Adiciona fuso horário correto

--- public/index.php
+++ public/index.php
@@ -10,6 +10,8 @@ require_once '../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(dirname(__DIR__, 1));
 $dotenv->safeLoad();
 
+date_default_timezone_set($_SERVER["TIMEZONE"]);
+
 $core = new Core(Auth::getSystem());
 
 $core->run();
