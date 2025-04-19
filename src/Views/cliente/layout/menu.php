0b80994412b26d31894db840f130060b045d8791
felipe <felipe>
2025-04-19 20:53:52 -0300

    feat: Adiciona lista de compras vencidas

--- src/Views/cliente/layout/menu.php
+++ src/Views/cliente/layout/menu.php
@@ -3,8 +3,9 @@
         Menu
     </header>
     <ul>
-        <li><a href="<?= $_SERVER["BASE_URL"] ?>dashboard">Inicio</a></li>
-        <li><a href="<?= $_SERVER["BASE_URL"] ?>compra">Minhas Compras</a></li>
-        <li><a href="<?= $_SERVER["BASE_URL"] ?>compra/pendente">Compras Pendentes</a></li>
+        <li><a href="<?=$_SERVER["BASE_URL"]?>dashboard">Inicio</a></li>
+        <li><a href="<?=$_SERVER["BASE_URL"]?>compra">Minhas Compras</a></li>
+        <li><a href="<?=$_SERVER["BASE_URL"]?>compra/pendente">Compras Pendentes</a></li>
+        <li><a href="<?=$_SERVER["BASE_URL"]?>compra/vencida">Compras Vencidas</a></li>
     </ul>
 </nav>
