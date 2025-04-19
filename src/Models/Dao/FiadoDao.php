0ffd6da9fc63732ec7228b30fa517a01073045fc
felipe <felipe>
2025-04-18 22:22:02 -0300

    refactor: Modifica o método total para receber condição

--- src/Models/Dao/FiadoDao.php
+++ src/Models/Dao/FiadoDao.php
@@ -39,45 +39,6 @@ class FiadoDao extends Model
      * @param ParamData $data
      * @return mixed
      */
-    public function listFiadoCliente(ParamData $data)
-    {
-        $statement = $this->select('fiado', 'id_loja = :id_loja AND id_cliente = :id_cliente', $data);
-
-        return $statement->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    /**
-     * @param string $condition
-     * @param ParamData $data
-     * @param ?string $limit
-     * @param ?string $orderBy
-     */
-    public function listFiadoPendente(string $condition, ParamData $data, ?string $limit = null, ?string $orderBy = null)
-    {
-        $condition = $condition . ($orderBy ? " ORDER BY {$orderBy}" : '');
-        $condition = $condition . ($limit ? " LIMIT {$limit}" : '');
-
-        $statement = $this->select('fiado, cliente', $condition, $data, 'fiado.*, cliente.name');
-
-        return $statement->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    /**
-     * @param string $condition
-     * @param ParamData $data
-     * @return mixed
-     */
-    public function countFiadoPendente(string $condition, ParamData $data)
-    {
-        $statement = $this->select('fiado, cliente', $condition, $data, 'COUNT(*)');
-
-        return $statement->fetch(\PDO::FETCH_COLUMN);
-    }
-
-    /**
-     * @param ParamData $data
-     * @return mixed
-     */
     public function getFiadoById(ParamData $data)
     {
         $statement = $this->select('fiado', 'id = :id', $data);
@@ -86,64 +47,15 @@ class FiadoDao extends Model
     }
 
     /**
+     * @param string $condition
      * @param ParamData $data
      * @return mixed
      */
-    public function total(ParamData $data)
-    {
-        $setPaid = array_filter($data->getData(), function ($paramItem) {
-            return $paramItem->getValue() === null;
-        });
-
-        $paid = '= :paid';
-        if ($setPaid) {
-            $paid = 'IN(:paid, 0, 1)';
-        }
-
-        $statement = $this->select(
-            'fiado',
-            "paid $paid AND id_loja = :id_loja AND date BETWEEN :start AND :end",
-            $data,
-            "SUM(`total`)"
-        );
-
-        return $statement->fetchColumn();
-    }
-
-    /**
-     * @param ParamData $data
-     * @return mixed
-     */
-    public function totalVencido(ParamData $data)
+    public function total(string $condition, ParamData $data)
     {
         $statement = $this->select(
             'fiado',
-            "due_date < :due_date AND id_loja = :id_loja AND paid = :paid AND due_date > :from",
-            $data,
-            "SUM(`total`)"
-        );
-
-        return $statement->fetchColumn();
-    }
-
-    /**
-     * @param ParamData $data
-     * @return mixed
-     */
-    public function totalCliente(ParamData $data)
-    {
-        $setPaid = array_filter($data->getData(), function ($paramItem) {
-            return $paramItem->getValue() === null;
-        });
-
-        $paid = '= :paid';
-        if ($setPaid) {
-            $paid = 'IN(:paid, 0, 1)';
-        }
-
-        $statement = $this->select(
-            'fiado',
-            "paid $paid AND id_loja = :id_loja AND id_cliente = :id_cliente AND date BETWEEN :start AND :end",
+            $condition,
             $data,
             "SUM(`total`)"
         );
