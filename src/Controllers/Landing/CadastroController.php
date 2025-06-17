8256aab3e2043613904987c7f1de21f310112872
felipe <felipe>
2025-06-17 02:54:17 -0300

    feat: Adiciona completar cadastro

--- src/Controllers/Landing/CadastroController.php
+++ src/Controllers/Landing/CadastroController.php
@@ -8,6 +8,7 @@ use Fiado\Enums\InputType;
 use Fiado\Helpers\Flash;
 use Fiado\Helpers\FormData;
 use Fiado\Models\Service\AuthService;
+use Fiado\Models\Service\ClienteCompletionLinkService;
 use Fiado\Models\Service\ClienteService;
 use Fiado\Models\Service\LojaService;
 
@@ -25,9 +26,6 @@ class CadastroController extends Controller
         $this->load('signup', $data);
     }
 
-    /**
-     * @return mixed
-     */
     public function salvar()
     {
         $this->checkToken($_SERVER["BASE_URL"]);
@@ -66,5 +64,126 @@ class CadastroController extends Controller
         }
 
         return $this->redirect($urlCadastro);
+    }
+
+    /**
+     * @param string $token
+     */
+    public function completar(string $token)
+    {
+        $hash = hash('sha256', urlencode($token) . $_SERVER["HASH_SALT"]);
+        $completion = ClienteCompletionLinkService::getClienteCompletionLink($hash) ?: null;
+
+        if (
+            $completion
+            && ($completion->getUsed() === false)
+            && ($completion->getValidUntil() > date('Y-m-d H:i:s', strtotime('now')))
+        ) {
+            $invalid = false;
+        }
+
+        if ($completion && ($completion->getUsed() === false)) {
+            $used = false;
+        }
+
+        $data['tokenEmail'] = $token;
+        $data['tokenInvalido'] = $invalid ?? true;
+        $data['tokenUsed'] = $used ?? true;
+        $data['view'] = 'completeAccount';
+
+        $this->load('completeAccount', $data);
+    }
+
+    public function completa()
+    {
+        $form = new FormData();
+
+        $form->setItem('tokenEmail')->getValueFrom('hidden-email-token');
+        $form->setItem('password')->getValueFrom('ipt-senha');
+        $form->setItem('conPassword')->getValueFrom('ipt-con-senha');
+
+        $urlReturn = $_SERVER["BASE_URL"] . 'cadastro/completar/' . urlencode($form->tokenEmail);
+
+        $this->checkToken($urlReturn);
+
+        $hash = hash('sha256', urlencode($form->tokenEmail) . $_SERVER["HASH_SALT"]);
+        $completion = ClienteCompletionLinkService::getClienteCompletionLink($hash);
+
+        if (!$completion) {
+            return $this->redirect($urlReturn);
+        }
+
+        $cliente = ClienteService::getClienteById($completion->getCliente()->getId());
+
+        if (!$cliente) {
+            return $this->redirect($urlReturn);
+        }
+
+        ClienteCompletionLinkService::getDao()->beginTransation();
+        ClienteService::getDao()->beginTransation();
+
+        if (!ClienteCompletionLinkService::salvar(
+                $completion->getId(),
+                $completion->getHash(),
+                $completion->getCliente()->getId(),
+                $completion->getLoja()->getId(),
+                true,
+                $completion->getValidUntil()
+            )
+            || !ClienteService::salvar(
+                $cliente->getId(),
+                $cliente->getCpf(),
+                $cliente->getName(),
+                $cliente->getEmail(),
+                $form->password,
+                $form->conPassword
+            )
+        ) {
+            ClienteCompletionLinkService::getDao()->rollback();
+            ClienteService::getDao()->rollback();
+
+            return $this->redirect($urlReturn);
+        }
+
+        ClienteCompletionLinkService::getDao()->commit();
+        ClienteService::getDao()->commit();
+
+        return $this->redirect($_SERVER["BASE_URL"] . 'auth');
+    }
+
+    public function reenviar()
+    {
+        $form = new FormData();
+
+        $form->setItem('tokenEmail')->getValueFrom('hidden-email-token');
+
+        $this->checkToken($_SERVER["BASE_URL"]);
+
+        $hash = hash('sha256', urlencode($form->tokenEmail) . $_SERVER["HASH_SALT"]);
+        $completion = ClienteCompletionLinkService::getClienteCompletionLink($hash);
+
+        if ($completion && ($completion->getUsed() === false)) {
+            ClienteService::getDao()->beginTransation();
+            ClienteCompletionLinkService::getDao()->beginTransation();
+
+            if (!ClienteService::sendEmailCompletionLink($completion->getCliente()->getId(), $completion->getLoja()->getId())
+                || !ClienteCompletionLinkService::salvar(
+                    $completion->getId(),
+                    $completion->getHash(),
+                    $completion->getCliente()->getId(),
+                    $completion->getLoja()->getId(),
+                    true,
+                    $completion->getValidUntil()
+                )
+            ) {
+                ClienteService::getDao()->rollback();
+                ClienteCompletionLinkService::getDao()->rollback();
+            }
+
+            ClienteService::getDao()->commit();
+            ClienteCompletionLinkService::getDao()->commit();
+        }
+
+        return $this->redirect($_SERVER["BASE_URL"]);
     }
 }
